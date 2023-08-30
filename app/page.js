565ed465bcebfb1de02a2a0eb26e1cde605978a2
Berke SASA <berkesasa@Berke-MacBook-Air.local>
2023-08-30 20:33:45 +0300

    third update

--- app/page.js
+++ app/page.js
@@ -5,15 +5,21 @@ import requests from './utils/requests';
 
 
 export default async function Page({ searchParams }) {
+  const API_KEY = process.env.API_KEY;
+
 
   //  const request = await fetch(`https://api.themoviedb.org/3/${searchParams.genre ? "movie/" + searchParams.genre : "trending/all/day"}`, {next: { revalidate: 10000}})
   // const genre = searchParams.get('genre');
   // const url = requests[genre].url
 
-  const getUrl = requests[searchParams.genre]
+
+  const getUrl = requests[searchParams.genre]  != null ? requests[searchParams.genre].url : `/trending/all/week?api_key=${API_KEY}&language=en-US`;
 
 
-  const request = await fetch(`https://api.themoviedb.org/3${getUrl.url}`
+  // const getUrl = requests[searchParams.genre].url
+
+
+  const request = await fetch(`https://api.themoviedb.org/3${getUrl}`
   ).then((res) => res.json());
 
   return (
