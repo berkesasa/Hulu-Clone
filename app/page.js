e96c0cef73ba1530ddbaa339dcc4c2e797a5581f
Berke SASA <berkesasa@Berke-MacBook-Air.local>
2023-08-31 13:21:57 +0300

    updatev222

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import requests from './utils/requests';
 
 
 export default async function Page({ searchParams }) {
+  const API_KEY = process.env.API_KEY;
 
   const getUrl = requests[searchParams.genre]  != null ? requests[searchParams.genre].url : `/trending/all/week?api_key=${API_KEY}&language=en-US`;
 
